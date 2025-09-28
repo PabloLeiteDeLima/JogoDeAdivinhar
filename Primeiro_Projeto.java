c640dc4453c48064954e1783b9b720d9fcb69672
Pablo Leite <pabloleitelima7@gmail.com>
2025-09-28 11:28:17 -0300

    Jogo Adivinha Número

--- Primeiro_Projeto.java
+++ Primeiro_Projeto.java
@@ -1,6 +1,6 @@
 package curso;
 /*
- * data: 27/09
+ * data: 27/09/2025
  * Crie um programa que simula um jogo de adivinhação, que deve gerar um número aleatório entre 0 e 100 
  * e pedir para que o usuário tente adivinhar o número, em até 5 tentativas. 
  * A cada tentativa, o programa deve informar se o número digitado pelo usuário é maior ou menor do que o número gerado.
